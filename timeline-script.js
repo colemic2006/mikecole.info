7563f575ffdacdd14002933be2680c560d875632
colemic2006 <251512021+colemic2006@users.noreply.github.com>
2026-03-04 20:18:01 -0600

    Add files via upload

---
+++ timeline-script.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll('.ptl-item');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.2 });
+
+items.forEach(item => {
+  observer.observe(item);
+});
